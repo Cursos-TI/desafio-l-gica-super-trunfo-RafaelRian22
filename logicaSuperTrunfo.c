fa123c2d6c2d18a8ef7fdb382eb957c2af3141a5
RafaelRian22 <rafaelriantavares123@gmail.com>
2025-11-06 00:17:47 +0000

    novo commit

--- logicaSuperTrunfo.c
+++ logicaSuperTrunfo.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
 int main() {
   // definição das variáveis
-  int populacao, populacao2 ,pontosturisticos,pontosturisticos2,comparacaopopulacional,comparacaopontosturisticos,comparacaopib,
-  comparacaopibpercapita,comparacaoarea,comparacaosuperpoder,comparacaodensidadepopulacional;
+  int populacao, populacao2 ,pontosturisticos,pontosturisticos2,comparacaopopulacional,comparacaopopulacional2,comparacaopontosturisticos,comparacaopontosturisticos2,
+  comparacaopib,comparacaopib2,comparacaopibpercapita,comparacaopibpercapita2,comparacaoarea,comparacaoarea2,comparacaosuperpoder,comparacaosuperpoder2,numero,
+  comparacaodensidadepopulacional,comparacaodensidadepopulacional2;
   float area,area2,pib,pib2,densidadepopulacional,densidadepopulacional2,pibpercapita,pibpercapita2,densidadepopulacionalinversa,
   densidadepopulacionalinversa2,superpoder,superpoder2;
   char nomedacidade[50],nomedacidade2[50],estado[10],estado2[10];
@@ -17,7 +18,7 @@ int main() {
   scanf("%s",nomedacidade);
   printf("digite o número da população dessa cidade:\n");
   scanf("%d", &populacao);
-  printf("digite a àrea em metros quadrado dessa cidade:\n");
+  printf("digite a àrea em km² dessa cidade:\n");
   scanf("%f", &area);
   printf("digite o pib dessa cidade:\n");
   scanf("%f", &pib);
@@ -31,7 +32,7 @@ int main() {
   scanf("%s",nomedacidade2);
   printf("digite o número da população dessa cidade:\n");
   scanf("%d", &populacao2);
-  printf("digite a àrea em metros quadrado dessa cidade:\n");
+  printf("digite a àrea em km² dessa cidade:\n");
   scanf("%f", &area2);
   printf("digite o pib dessa cidade:\n");
   scanf("%f", &pib2);
@@ -48,62 +49,104 @@ int main() {
   superpoder2 = (float)(area2+pib2+pibpercapita2+populacao2+densidadepopulacionalinversa2+pontosturisticos2+area2);
 
   // Área para exibição dos dados da cidade
-  printf("\ncarta 1: estado:%s\n código:%s01\n nome da cidade: %s\n população:%d\n PIB:%f\n pontosturísticos:%d\n área: %fm²\n pi per capita:%f\n densidade populacional: %f\n superpoder: %f\n", estado,estado,nomedacidade,
+  printf("\ncarta 1: estado:%s\n código:%s01\n nome da cidade: %s\n população:%d\n PIB:%f\n pontosturísticos:%d\n área: %fkm²\n pi per capita:%f\n densidade populacional: %f\n superpoder: %f\n", estado,estado,nomedacidade,
     populacao,pib,pontosturisticos,area,pibpercapita,densidadepopulacional,superpoder);
 
-  printf("\ncarta 2: estado:%s\n código:%s02\n nome da cidade: %s\n população:%d\n PIB:%f\n pontosturísticos:%d\n área: %fm²\n pi per capita:%f\n densidade populacional:%f\n superpoder: %f\n", estado2,estado2,nomedacidade2,
+  printf("\ncarta 2: estado:%s\n código:%s02\n nome da cidade: %s\n população:%d\n PIB:%f\n pontosturísticos:%d\n área: %fkm²\n pi per capita:%f\n densidade populacional:%f\n superpoder: %f\n", estado2,estado2,nomedacidade2,
     populacao2,pib2,pontosturisticos2,area2,pibpercapita2,densidadepopulacional2,superpoder2);
 
   //Área de comparação de cartas
   comparacaopopulacional = populacao > populacao2;
+  comparacaopopulacional2 = populacao < populacao2;
   comparacaoarea = area > area2;
+  comparacaoarea2 = area < area2;
   comparacaopib = pib > pib2;
+  comparacaopib2 = pib < pib2;
   comparacaopontosturisticos = pontosturisticos > pontosturisticos2;
+  comparacaopontosturisticos2 = pontosturisticos < pontosturisticos2;
   comparacaopibpercapita = pibpercapita > pibpercapita2;
+  comparacaopibpercapita2 = pibpercapita < pibpercapita2;
   comparacaodensidadepopulacional = densidadepopulacional < densidadepopulacional2;
+  comparacaodensidadepopulacional2 = densidadepopulacional > densidadepopulacional2;
   comparacaosuperpoder = superpoder > superpoder2;
-
-  // Área de exibição de exibição das comparações das cartas
-  if(comparacaopopulacional){
+  comparacaosuperpoder2 = superpoder < superpoder2;
+  //menu
+     //entrada de dados
+  printf("\nmenu principal\n escolha uma propriedade:\n");
+  printf("digite 1 para escolher a propriedade população:\ndigite 2 para escolher a propriedade área:\n");
+  printf("digite 3 para escolher a pib:\ndigite 4 para escolher a propriedade pontos turísticos:\n");
+  printf("digite 5 para escolher a propriedade pib per capita :\ndigite 6 para escolher a propriedade densidade populacional:\n");
+  printf("digite 7 para escolher a propriedade super poder:\n");
+    scanf("%d",&numero);
+    //comparações das cartas
+   switch (numero)
+   {
+   case 1:
+     if(comparacaopopulacional){
     printf("\npopulação:carta1 venceu\n");
+  }else if(comparacaopopulacional2){
+    printf("população:carta2 venceu\n");
   } else{
-   
-    printf("\npopulação:carta2 venceu\n");
+    printf("população:empatado\n");
   }
-   if(comparacaoarea){
+    break;
+   case 2: 
+    if(comparacaoarea){
     printf("área:carta1 venceu\n");
-  } else{
-
+  }else if(comparacaoarea2){
     printf("área:carta2 venceu\n");
+  } else{
+    printf("área:empatado\n");
   }
-   if(comparacaopib){
+     break;
+   case 3:
+     if(comparacaopib){
     printf("pib:carta1 venceu\n");
-  } else{
- 
+  }else if(comparacaopib2){
     printf("pib:carta2 venceu\n");
-  }
-   if(comparacaopontosturisticos){
-    printf("pontos turístico:carta1 venceu\n");
   } else{
-
+    printf("pib:empatado\n");
+  } 
+     break;
+   case 4:
+     if(comparacaopontosturisticos){
+    printf("pontos turísticos:carta1 venceu\n");
+  }else if(comparacaopontosturisticos2){
     printf("pontos turísticos:carta2 venceu\n");
+  } else{
+    printf("pontos turísticos:empatado\n");
   }
-   if(comparacaopibpercapita){
+   break;
+   case 5:
+     if(comparacaopibpercapita){
     printf("pib per capita:carta1 venceu\n");
-  } else{
-
+  }else if(comparacaopibpercapita2){
     printf("pib per capita:carta2 venceu\n");
-  }
-   if(comparacaodensidadepopulacional){
+  } else{
+    printf("pib per capita:empatado\n");
+  } 
+     break;
+   case 6:
+     if(comparacaodensidadepopulacional){
     printf("densidade populacional:carta1 venceu\n");
-  } else{
-
+  }else if(comparacaodensidadepopulacional2){
     printf("densidade populacional:carta2 venceu\n");
-  }
-   if(comparacaosuperpoder){
-    printf("super poder:carta1 venceu\n");
   } else{
-    printf("super poder:carta2 venceu\n");
+    printf("densidade populacional:empatado\n");
   }
-  return 0;
+     break;
+   case 7:
+     if(comparacaosuperpoder){
+    printf("super poder:carta1 venceu\n");
+  }else if(comparacaosuperpoder2){
+    printf("super poder:carta2 venceu\n");
+  } else{
+    printf("super poder:empatado\n");
+  }
+     break;
+     default:
+     printf("falha! você não digitou nenhuma das opções disponivel.\n");
+     break;
+   }
+   return 0;
 } 
